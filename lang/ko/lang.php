6aa688de05e6f680ac6956153f6c308734efe200
Elan Ruusamäe <glen@delfi.ee>
2012-06-12 01:03:21 +0300

    add Korean translations
    Source: http://www.dokuwiki.org/plugin:page_redirector?rev=1327652809

---
+++ lang/ko/lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Traditional Korean for Page Redirect plugin
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author:     wincomi <http://forum.dokuwiki.org/user/8622>
+ */
+
+$lang['redirect_to']     = "이 페이지는 옮겨졌습니다. 새로운 페이지는 %s입니다.";
+$lang['redirected_from'] = "%s에서 넘어왔습니다.";
---
+++ lang/ko/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Traditional Korean for Page Redirect plugin
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author:     wincomi <http://forum.dokuwiki.org/user/8622>
+ */
+
+$lang['show_note'] = '리디렉션되는 페이지에 메모를 표시할까요?';
