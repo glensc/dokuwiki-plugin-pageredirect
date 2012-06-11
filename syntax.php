70243faaa76d72d81d3e8829ee70dc4263bb2fec
Elan Ruusamäe <glen@delfi.ee>
2012-06-11 22:27:16 +0300

    load inc/html.php for older dokuwikis

--- syntax.php
+++ syntax.php
@@ -10,6 +10,7 @@ if(!defined('DOKU_INC')) die();
 
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/'); 
 require_once(DOKU_PLUGIN.'syntax.php'); 
+require_once(DOKU_INC.'inc/html.php');
 
 /** 
  * All DokuWiki plugins to extend the parser/rendering mechanism 
