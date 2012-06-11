e22f33a1d9923633db8b1e9700ceeffdcde68a47
Elan Ruusamäe <glen@delfi.ee>
2012-06-12 01:06:06 +0300

    add portugese translations
    source: http://www.dokuwiki.org/plugin:page_redirector?rev=1334303478

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Portuguese-br language for Page Redirect plugin
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author:     Juliano Cezar <jcp%40live.de>
+ */
+
+$lang['redirect_to']     = "Esta página é um redirecionador para %s.";
+$lang['redirected_from'] = "Você foi redirecionado de %s.";
