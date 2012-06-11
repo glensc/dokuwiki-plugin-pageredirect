82539aef1b171ae50555d4872c44ee7637c75e91
Elan Ruusamäe <glen@delfi.ee>
2012-06-12 00:59:25 +0300

    add zh-tw translations
    source: http://www.dokuwiki.org/plugin:page_redirector?rev=1293598880

---
+++ lang/zh-tw/lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Traditional Chinese language for Page Redirect plugin
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author:     Danny Lin <danny0838[at]pchome[dot]com[dot]tw>
+ */
+
+$lang['redirect_to']     = "本頁已重定向至 %s。";
+$lang['redirected_from'] = "本頁重定向自 %s。";
---
+++ lang/zh-tw/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Traditional Chinese for Page Redirect plugin
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author:     Danny Lin <danny0838[at]pchome[dot]com[dot]tw>
+ */
+
+$lang['show_note'] = '在重定向目標頁顯示提示？';
