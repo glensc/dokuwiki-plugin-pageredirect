d77260fb533073932e61da720f556d6bcbc9f3cb
Philipp A. Hartmann <philipp.hartmann@offis.de>
2013-07-21 09:40:33 +0200

    make ~~ redirect pattern non-greedy (closes #5)

--- syntax.php
+++ syntax.php
@@ -23,7 +23,7 @@ class syntax_plugin_pageredirect extends DokuWiki_Syntax_Plugin {
 	function getSort() { return 1; }
 
 	function connectTo($mode) {
-		$this->Lexer->addSpecialPattern('(?:~~REDIRECT>.+~~|^#(?i:redirect) [^\r\n]+)', $mode, 'plugin_pageredirect');
+		$this->Lexer->addSpecialPattern('(?:~~REDIRECT>.+?~~|^#(?i:redirect) [^\r\n]+)', $mode, 'plugin_pageredirect');
 	}
 
 	/**
