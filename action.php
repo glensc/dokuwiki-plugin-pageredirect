29a6d85ca4798efd198d396067c1d639997f02c8
Elan Ruusamäe <glen@delfi.ee>
2014-04-14 21:11:51 +0300

    fix access to protected variable after splitbrain/dokuwiki#555. fixes #10, #11

--- action.php
+++ action.php
@@ -18,13 +18,18 @@ require_once(DOKU_PLUGIN.'action.php');
 class action_plugin_pageredirect extends DokuWiki_Action_Plugin {
 	public function register(Doku_Event_Handler $controller) {
 		$controller->register_hook('DOKUWIKI_STARTED', 'BEFORE', $this, 'handle_pageredirect_redirect');
-		$controller->register_hook('TPL_ACT_RENDER', 'BEFORE', $this, 'handle_pageredirect_note');
 		$controller->register_hook('PARSER_METADATA_RENDER','BEFORE', $this, 'handle_pageredirect_metadata');
 
 		// This plugin goes first
-		$hooks =& $controller->_hooks[TPL_ACT_RENDER_BEFORE];
-		if ($hooks[0][0] != $this) {
-			array_unshift($hooks, array_pop($hooks));
+		// After PR#555
+		$controller->register_hook('TPL_ACT_RENDER', 'BEFORE', $this, 'handle_pageredirect_note', null, -PHP_INT_MAX);
+
+		// Before PR#555, i.e 2013-12-08 release
+		if (isset($controller->_hook)) {
+			$hooks =& $controller->_hooks[TPL_ACT_RENDER_BEFORE];
+			if ($hooks[0][0] != $this) {
+				array_unshift($hooks, array_pop($hooks));
+			}
 		}
 	}
 
