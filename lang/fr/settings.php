74b16c9e140efd78cd434a39518893d58dca9fdb
Cinlloc Melnec <cinlloc@gmail.com>
2014-09-11 16:04:58 +0200

    French language added
    Add french translation php files

---
+++ lang/fr/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * French language for Page Redirect plugin
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author:     Cinlloc Melnec <cinlloc@gmail.com>
+ */
+
+$lang['show_note'] = 'Afficher une note sur la page vers laquelle vous êtes redirigé ?';
