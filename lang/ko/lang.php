5ddb8cb3bebae5be55d3b742a60b850b47781391
Myeongjin <aranet100@gmail.com>
2015-07-25 21:44:11 +0900

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * Traditional Korean for Page Redirect plugin
+ * Korean language for Page Redirect plugin
  *
  * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author:     wincomi <http://forum.dokuwiki.org/user/8622>
+ * @author:     Myeongjin <aranet100@gmail.com>
  */
 
-$lang['redirect_to']     = "이 페이지는 옮겨졌습니다. 새로운 페이지는 %s입니다.";
+$lang['redirect_to']     = "이 문서는 옮겨졌으며, 새 위치는 %s입니다.";
 $lang['redirected_from'] = "%s에서 넘어왔습니다.";
