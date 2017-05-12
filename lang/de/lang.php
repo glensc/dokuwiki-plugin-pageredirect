9a441d1607e67d382914cc7d716da61fc7463897
Oliver Völker <info@ovtec.it>
2017-05-12 10:15:14 +0200

    Added german translation

---
+++ lang/de/lang.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * German language for Page Redirect plugin
+ *
+ * @license: GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author : Oliver Völker <info@ovtec.it>
+ */
+
+$lang['redirect_to']     = "Diese Seite wurde verschoben, die neue URL lautet %s.";
+$lang['redirected_from'] = "Sie wurden von %s hierher umgeleitet.";
