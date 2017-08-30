8d1ceacb7686343a1c01194970eeceacbf7bc749
Reedych Coyote <reedych@reedych.pw>
2017-08-30 23:58:47 +0600

    Esperanto translation

---
+++ lang/eo/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Esperanto language for Page Redirect plugin
+ * Lingvo Esperanto por kromaĵo Page Redirect
+ *
+ * @license:    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author :    Reedych <reedych@ya.ru>
+ */
+
+$lang['show_note'] = 'Montri rimarkon sur paĝo ke vi estas alidirektita?';
