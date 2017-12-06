c559de2b10474d633da8dedc0062b98d57928208
Eike Broda <git@ebroda.de>
2017-12-06 04:27:21 +0100

    Add informal german language

---
+++ lang/de-informal/settings.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * German language for Page Redirect plugin
+ *
+ * @license: GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author : Oliver Völker <info@ovtec.it>
+ */
+
+$lang['show_note'] = 'Auf der Zielseite einen Hinweis zur Umleitung anzeigen?';
